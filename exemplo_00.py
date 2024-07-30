5152b17a7892c20a98419feec0f194d7e0c097a5
larissa.sena <larissa.dourados@outlook.com.br>
2024-07-30 00:17:59 -0300

    add exemplo_00.py file

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,1 +1,13 @@
-print("Hello Word!!!!!")
+# Captura as entradas do usuário
+nome = input("Digite seu nome: ")
+salario = float(input("Digite seu salario: "))
+bonus = float(input("Digite seu último bônus: "))
+
+# Define a função para calcular o bônus
+def calculadora_bonus(salario, bonus):
+    bonus_calculado = 1000 + (salario * bonus)
+    return bonus_calculado
+
+# Usa a função e imprime o resultado
+bonus_total = calculadora_bonus(salario, bonus)
+print(f"O bônus total calculado para {nome} é: {bonus_total:.2f}")
